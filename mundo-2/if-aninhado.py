473b81debdb9b1203beeedf2a046f3bfbd8d51f5
Francisco Aparicio <115089730+euapariciocoelho@users.noreply.github.com>
2022-10-20 09:20:33 -0300

    Initial commit

---
+++ mundo-2/if-aninhado.py
@@ -0,0 +1,11 @@
+nome = str(input("Nome: ")).lower()
+
+if nome == 'aparicio':
+    print("Seu nome é muito bonito!")
+elif nome == 'enzo' or 'maria':
+    print("Seu nome é bem popular")
+else:
+    print("Prazer {}".format(nome))
+
+
+'''bin(var), oct(var), hex(var)'''
