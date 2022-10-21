037107c6b0fb4a605ce93184c9054a96e564c5ae
Francisco Aparicio <115089730+euapariciocoelho@users.noreply.github.com>
2022-10-21 09:59:37 -0300

    Add files via upload

---
+++ mundo-2/for.py
@@ -0,0 +1,10 @@
+'''0 a 4'''
+for i in range(5):
+    print('Oi')
+'''definir inicio e fim'''
+for i in range(1, 6):
+    print('Olá')
+'''definir inicio, fim e iteração'''
+'''comeã em 1, vai até 10, de 2 em 2, se fosse negativo seria -2...'''
+for i in range(1, 10, 2):
+    print(i)
